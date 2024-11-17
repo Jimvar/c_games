f1586a84a10295618e6eae7970254879acce7e45
Viktor Varoutis <jingleinternet@gmail.com>
2024-11-17 20:57:51 +0200

    Enhance startup screen and add leaderboard functionality

--- blackjack.c
+++ blackjack.c
@@ -7,24 +7,72 @@
 
 
 int startupscreen(){
+    char c;
     FILE *fp;
+
     fp = fopen("savegame.txt", "r");
     if(fp==NULL){
         printf("File not found, exiting...\n");
         exit(EXIT_FAILURE);
     }
-    printf("Blackjack\n");
-    char c;
-    printf("Choose:\n");
+    int flag = fscanf(fp, "%c", &c);
+    fclose(fp);
+
+    printf("Blackjack\nChoose:\n");
     printf(GREEN "1. New Game\n");
-    (fscanf(fp, "%c", &c) != EOF) ? printf("2. Continue\n") : printf(RED "2. Continue\n");
+    flag != -1 ? printf("2. Continue\n") : printf(RED "2. Continue\n");
     printf(GREEN "3. Leaderboard\n" RESET);
+
+    if(flag!=-1){
+        do{
+            c = getchar();
+        } while(c<'1' || c>'3');
+    }
+    else{
+        do{
+            c = getchar();
+        } while(c!='1' && c!='3');
+    }
+
+    return(c - '0');
+}
+
+void leaderboard(){
+    FILE *fp;
+    int board[10];
+
+    fp = fopen("leaderboard.txt", "r");
+    if(fp==NULL){
+        printf("File not found, exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    printf("------------------------------------\nLEADERBOARD\n");
+    for(int i = 0; i<10; i++){
+        fscanf(fp, "%d", &board[i]);
+        printf("%d. %d\n", i+1, board[i]);
+    }
+    printf("------------------------------------\n");
     fclose(fp);
 }
 
 int main(){
     int choice = startupscreen();
 
+    while(1){
+        if(choice==3){
+            leaderboard();
+            choice = startupscreen();
+            continue;
+        }
+        else if(choice==2){
+
+        }
+        else{
+
+        }
+    }
 
 
     
