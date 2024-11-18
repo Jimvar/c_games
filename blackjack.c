7d55a3afe2d77a179e1d920acdc92ec4f7b5fdb6
Viktor Varoutis <92715836+Jimvar@users.noreply.github.com>
2024-11-18 18:04:18 +0200

    Update blackjack.c
    Refined new game and continue

--- blackjack.c
+++ blackjack.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define GREEN "\x1b[1;32m"
 #define RED     "\x1b[1;31m"
@@ -16,7 +17,7 @@ void first_time_set(){
 
     fp = fopen("leaderboard.txt", "r");
     if(fp==NULL){
-        fp = fopen('leaderboard.txt', "w");
+        fp = fopen("leaderboard.txt", "w");
         fprintf(fp, "LEADERBOARD\n");
         for(int i = 0; i<10; i++){
             fprintf(fp, "%d. %d\n", i+1, 0);
@@ -76,18 +77,43 @@ void leaderboard(){
     fclose(fp);
 }
 
-void new_game(char name[]){
-    printf("Write your name: ");
-    scanf("%49s", name);
-
+void new_game(char name[], int *money, int *seed, int *seedflag){
     FILE *fp;
     fp = fopen("savegame.txt", "w");
-    fprintf(fp, "%s %d", name, 1000);
+
+    printf("Write your name: ");
+    scanf("%49s", name);
+    *money = 1000;
+    fprintf(fp, "%s %d ", name, *money);
+    
+    printf("Do you want to manually set a seed or have it random? Press 1 or 2: ");
+    int answer;
+    do{
+        scanf("%d", &answer);
+    } while(answer!=1 || answer!=2);
+    
+    if(answer==1){
+        printf("Write your seed: ");
+        scanf("%d", &answer);
+        *seed = answer;
+        *seedflag = 1;
+        fprintf(fp, "%d %d", *seed, *seedflag);
+    }
+    else{
+        *seed = TIME(NULL);
+        *seedflag = 0;
+        fprintf(fp, "%d %d", *seed, *seedflag);
+    }
+
+    fclose(fp);
 }
 
 int main(){
     first_time_set();
     char name[50];
+    int money;
+    int seed, seedflag;
+    FILE *fp;
     int choice = startupscreen();
 
     while(1){
@@ -97,13 +123,12 @@ int main(){
             continue;
         }
         else if(choice==1){
-            new_game(name);
+            new_game(name, &money, &seed, &seedflag);
         }
         else if(choice==2){
-
+            fp = fopen("savegame.txt", "r");
+            fscanf(fp, "%s %d %d %d", name, &money, &seed, &seedflag);
+            fclose(fp);
         }
     }
-
-
-    
 }
